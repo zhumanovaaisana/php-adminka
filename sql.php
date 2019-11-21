095e64d5eb90c4cd621b2fda17c15d9a38563b46
zhumanovaaisana <zhumanova.aisana@gmail.com>
2019-11-21 23:23:29 +0600

    rel1

---
+++ sql.php
@@ -0,0 +1,8 @@
+SELECT * FROM `users`
+
+
+INSERT INTO `users` (`id`, `name`, `email`, `date`, `status`) VALUES (NULL, 'Deni Targariyen', 'deni@got.com', '2019-10-14', '1')
+
+DELETE * FROM `users`
+
+UPDATE `users` SET `name`=[deni Targariyen] WHERE `id`=1 
